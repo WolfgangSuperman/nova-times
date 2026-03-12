f8543197ac879887892cc0beffd0b6a6fc627c90
Owen Henry <61534213+WolfgangSuperman@users.noreply.github.com>
2026-03-12 15:57:43 -0400

    flexible titling
    viz.py will now pull the unique "Star Names" from the input AAVSO datatable and use them to set the title of the lightcurve plot. works for the viz function by itself and across any use of viz_dataset in the measure function for all algorithms.

--- src/nova_times/viz.py
+++ src/nova_times/viz.py
@@ -3,14 +3,21 @@ from typing import Optional
 from numpy.typing import NDArray
 
 from astropy.table import Table
+from astropy.table import unique
+
 from matplotlib.axes import Axes
 
 from matplotlib.lines import Line2D
 
 def viz_dataset(ax: Axes, data_table: Table, band: Optional[str] = None, lims: Optional[NDArray] = None) -> None:
 
-    marker_keys = list(Line2D.markers.keys())
+    marker_keys = list(Line2D.markers.keys()) #matplotlib markers 
+
+    names = list(unique(data_table, keys=['Star Name'])['Star Name'])
+    clean_names = [str(name) for name in names] #star names in table for title
+    
     for marker_indx, group in enumerate(data_table.groups):
+        #print(group)
         band_label = group[0]["Band"]
         if band is None or band_label == band:
             ax.scatter(
@@ -27,9 +34,6 @@ def viz_dataset(ax: Axes, data_table: Table, band: Optional[str] = None, lims: O
     if lims is not None:
         ax.set_xlim(lims[0], lims[1])
     
-    
-    ax.set_title("Light Curve of the Nova")
-    #ax.legend()
-    
+    ax.set_title("Lightcurve of "+", ".join(clean_names), wrap=True, fontsize=10)    
        
     return
